153429ad02e4fcc18276d8e8593523d83bb5e14f
Matrika Gupta <gupta.matrika@gmail.com>
2017-04-17 16:47:42 -0500

    Adding file

---
+++ 83_RemoveDuplicateFromSortedList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeInterview
+    {
+    class _83_RemoveDuplicateFromSortedList
+        {
+        public ListNode DeleteDuplicates(ListNode head)
+            {
+            ListNode current = head;
+            while (current != null)
+                {
+                if (current.next != null && current.val == current.next.val)
+                    current.next = current.next.next;
+                else
+                    current = current.next;
+                }
+            return head;
+            }
+        }
+    }
