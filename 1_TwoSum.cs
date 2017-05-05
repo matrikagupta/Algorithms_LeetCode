0efb56780ca68431a3fc9f701b58cb0c75608cf6
Matrika Gupta <gupta.matrika@gmail.com>
2017-05-05 16:52:19 -0500

    remove spaces

--- 1_TwoSum.cs
+++ 1_TwoSum.cs
@@ -5,38 +5,37 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace PracticeInterview
-{
+    {
     class _1_TwoSum
-    {           
-        public int[] twoSum(int[] nums, int target)
         {
+        public int[] twoSum(int[] nums, int target)
+            {
             var dict = new Dictionary<int, int>();
             for (int i = 0; i < nums.Length; i++)
-            {
+                {
                 var aim = target - nums[i]; //check aim value in dictionary
                 if (dict.ContainsKey(aim))
-                {
+                    {
                     return new int[] { dict[aim], i }; //return value from dictionary and current index of array number
-                }
+                    }
                 else
-                {
+                    {
                     if (!dict.ContainsKey(nums[i]))
                         dict.Add(nums[i], i); //add number as key and its index as value
+                    }
                 }
+            return null;
             }
 
-            return null;
+
+
         }
-
-        
-
-    }
 
 
 
     //class Program
     //{
-  
+
     //    static void Main(string[] args)
     //    {
     //        int[] a = { 3,5,3,2,7,3, 15 };
@@ -45,9 +44,9 @@ namespace PracticeInterview
     //        int[] u = obj.twoSum(a, target);
     //        Console.WriteLine(string.Join(",", u));
     //        Console.ReadLine();
-            
+
 
     //    }
-            
+
     //}
-}
+    }
