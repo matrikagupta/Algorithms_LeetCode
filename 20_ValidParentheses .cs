ea5b0720b3c4d1009d54399179566f2f9e488b39
Matrika Gupta <gupta.matrika@gmail.com>
2017-05-05 17:13:21 -0500

    valid paraentheses

---
+++ 20_ValidParentheses .cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeInterview
+    {
+    class _20_ValidParentheses
+        {
+        public bool IsValid(string s)
+            {
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in s.ToCharArray())
+                {
+                if (c == '(')
+                    stack.Push(')');
+                else if (c == '{')
+                    stack.Push('}');
+                else if (c == '[')
+                    stack.Push(']');
+                else if (stack.Count == 0 || stack.Pop() != c)
+                    return false;
+                }
+            return stack.Count == 0;
+            }
+
+        }
+    }
